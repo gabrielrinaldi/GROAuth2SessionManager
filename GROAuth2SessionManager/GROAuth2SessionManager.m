b294b35e2769fbb1e14dc14187778c9a0c2dfd44
Matt Gillingham <mgillingham@mediweb.jp>
2014-05-08 18:32:22 +0900

    Expose the session configuration.

--- GROAuth2SessionManager/GROAuth2SessionManager.m
+++ GROAuth2SessionManager/GROAuth2SessionManager.m
@@ -58,9 +58,13 @@ NSString * const kGROAuthRefreshGrantType = @"refresh_token";
 }
 
 - (id)initWithBaseURL:(NSURL *)url oAuthURL:(NSURL *)oAuthURL clientID:(NSString *)clientID secret:(NSString *)secret {
+    return [self initWithBaseURL:url sessionConfiguration:nil oAuthURL:oAuthURL clientID:clientID secret:secret];
+}
+
+- (id)initWithBaseURL:(NSURL *)url sessionConfiguration:(NSURLSessionConfiguration *)configuration oAuthURL:(NSURL *)oAuthURL clientID:(NSString *)clientID secret:(NSString *)secret {
     NSParameterAssert(clientID);
 
-    self = [super initWithBaseURL:url];
+    self = [super initWithBaseURL:url sessionConfiguration:configuration];
     if (self) {
         [self setServiceProviderIdentifier:[[self baseURL] host]];
         [self setClientID:clientID];
